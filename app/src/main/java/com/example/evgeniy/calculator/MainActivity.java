61c000b20cfcd1caeb265522a4c15b7aafe933f9
empoluboyarov <poluboyarovem@gmail.com>
2016-03-29 21:47:12 +0300

    Version 0.02

--- app/src/main/java/com/example/evgeniy/calculator/MainActivity.java
+++ app/src/main/java/com/example/evgeniy/calculator/MainActivity.java
@@ -2,12 +2,42 @@ package com.example.evgeniy.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText txtResult = (EditText)findViewById(R.id.txtResult);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buttonClick(View view){
+        switch (view.getId()){
+            case R.id.butAdd:
+                break;
+            case R.id.butDevide:
+                break;
+            case R.id.butMultiply:
+                break;
+            case R.id.butSubtract:
+                break;
+            case R.id.butComma:
+                break;
+            case R.id.butDelete:
+                break;
+            case R.id.butClear:
+                break;
+            case R.id.butResult:
+                break;
+
+            default:{
+                txtResult.setText(txtResult.getText()+view.getContentDescription().toString());
+            }
+
+        }
     }
 }
